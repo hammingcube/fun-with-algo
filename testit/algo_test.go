d792c59001e7c7e48032e17a7b8ca41023e3e57e
maddyonline <madhavjha@gmail.com>
2015-08-21 00:04:56 -0700

    more

--- testit/algo_test.go
+++ testit/algo_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestAverage(t *testing.T) {
 	p1 := pipe.Line(
-		pipe.ReadFile("../solutions/input01.txt"),
+		pipe.ReadFile("../solutions/input.txt"),
 	)
 	got, err := pipe.CombinedOutput(p1)
 	if err != nil {
