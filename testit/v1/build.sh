7e0b8d1d159f1f356f0d5d6f0b869cab31ce643f
maddyonline <madhavjha@gmail.com>
2015-09-19 19:47:24 -0700

    more

--- testit/v1/build.sh
+++ testit/v1/build.sh
@@ -1,3 +1,4 @@
+go build runtest.go
 g++ -std=c++11 can_string_be_palindrome_soln1.cc -o soln1
 g++ -std=c++11 can_string_be_palindrome_soln2.cc -o soln2
 g++ -std=c++11 Can_string_be_palindrome_gen.cc -o gen
